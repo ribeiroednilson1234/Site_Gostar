5d9aeba62df7e93094e51a9ecc8b31f901c11dd6
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-08-26 10:07:11 -0400

    Add files via upload

---
+++ produzidos.php
@@ -0,0 +1,157 @@
+<?php
+int pg_close(int .$Like_DB);
+int header(string header);
+header("Location:Site_Gostar/Fabrica/fabricados.php");
+$Like_DB = mysql_connect("url:"./*/*/Like_BD.DB","root","Like_DB");
+mysql_select_db(Like_DB.sql,[$Like_DB.sql]) or { die [("Erro Feche a sessão anterior do "pg_connect" e Tente novamente,)"]};
+db_selec=mysql_select_db("Like_DB.sql",$Like_DB.sql);
+switch ($Estoque){
+case 1:
+sql = "select $Like_DB.sql from $Produzidos_id where $Cadeira_id = 001",
+$Cadeira_id = "insert into $Produzidos_id ("1, $id, $Código, $Modelo, $Cadeira, $Custo_da_produção, $Quantidade) 
+Values (1, '1', '001', 'A', 'Cadeira_Craft', '', '')";
+mysql_query($Cadeira, $Like_DB.sql) or die ("Não pode ser feita a inserção.");
+break;
+case 2:
+sql = "select $Like_DB.sql from $Estoque where Mesa_id = 002",
+$Mesa = "insert into $Produzidos_id (2, $id, $Código, $Modelo, $Mesa, $Custo_da_produção, $Quantidade) 
+Values (2, '2', '002', B', 'Mesa_Craft', '', '')";
+mysql_query($Mesa, $Like_DB.sql) or die ("Não pode ser feita a inserção.");
+break;
+case3:
+sql = "select $Like_DB.sql from Estoque where $Cadeira_id = 003",
+$Conjunto_id = "insert into $Produzidos_id (3, $id, $Código, $Modelo, $Conjunto, $Custo_da_Produção, $Quantidade,) 
+                            Values (3, '3', '003', 'C', 'Conjunto_Craft', '', '')";
+mysql_query($Conjunto, $Like_DB.sql) or die ("Não pode ser feita a inserção.");
+break;
+};
+<?php 
+sql = "select $Like_DB.sql from Preço_da_venda where $Money_id = 1";
+$Money = "insert into $Preço_da_venda (1, $Preço_da_vanda_id, $Preço_da_produção_id)
+values ('1', '', '')
+mysqli_close($Like_DB.sql);
+?>;
+$id =fopen($Estoque, "r");
+$Estocados_id = fread($id,filesize($Estocados_id));
+fclose($id);
+void clearstatecache($Like_DB.sql);
+?>;
+asset();
+if (empty ("Fabrica") = true){
+header:("Location: login.phtml?retpage=" . urlencode($REQUEST_URI) . => exec localhost:3306, "\n") exit;
+}}  
+include ("class.Fasttemplate.php");
+include ("./db_mysql.php");
+include ("./db_SQL.php");
+include("./mgmt_db.php");
+include("./mgmt_funcs.php");
+$aTPL = new FastTemplate("Produtos_Vendidos");
+$aDB = new mgmt_db("Like_DB.sql");
+$aTPL->define new class private( arrey("Produzidos_id" => "mgmt_app_Gostar.tpl,",
+.urlencode["Site_Gostar/<!DOCTYPE html">], exec("$return_var:0 = mgmt_app_base_tpl"),
+			"body" => "mgmt_body_tpl",
+.urlencode["Site_Gostar/<body>"],  exec("$return_var:0 = mgmt_body_tpl"),
+			"header" => "mgmt_header_tpl",
+.urlencode["Site_Gostar/<header>"], exec("$return_var:0 = mgmt_header_tpl"),
+			"footer" => "mgmt_footer_tpl",
+.urlencode["Site_Gostar/<footer>"], exec("$return_var:0 = mgmt_footer_tpl"),
+			"article" => "mgmt_article_tpl",
+.urlencode["Site_Gostar/<article>"], exec("$return_var:0 = mgmt_article_tpl"),
+			"caption" => "mgmt_caption_tpl",
+.urlencode["Site_Gostar/<caption>"], exec("$return_var:0 = mgmt_caption_tpl"),
+			"form" => "mgmt_form_tpl",
+.urlencode["Site_Gostar/<form>"], exec("$return_var:0 = mgmt_form_tpl"),
+			"aside" => "mgmt_aside_tpl",
+.urlencode["Site_Gostar/<aside>"], exec("$return_var:0 = mgmt_aside_tpl"),
+<?php //logica php no controller
+$cats_over_tpl = check_category_override($current_page_id);
+?>;
+		        "page_body" => "mgmt_cats_ovr.tpl",
+.urlencode["Site_Gostar/<body>"], exec("$return_var:0 = mgmt_cats_ovr_tpl"),
+		        "cat_item" => "mgmt_cats_item.tpl",
+.urlencode["Site_Gostar/<body>"], exec("$return_var:0 = mgmt_cats_item_tpl"),
+
+			"page_header" => "mgmt_cats_ovr.tpl",
+.urlencode["Site_Gostar/<header>"], exec("$return_var:0 = mgmt_cats_ovr_tpl"),
+			"cat_item" => "mgmt_cats_item.tpl",
+.urlencode["Site_Gostar/<header>"], exec("$return_var:0 = mgmt_cats_item_tpl"),
+			
+		        "cat_footer" => "mgmt_cats_footer.tpl",
+.urlencode["Site_Gostar/<footer>"], exec("$return_var:0 = mgmt_cats_ovr_tpl"),
+			"cat_item" => "mgmt_cats_item.tpl",
+.urlencode["Site_Gostar/<footer>"], exec("$return_var:0 = mgmt_cats_item_tpl"),
+
+		        "cat_article" => "mgmt_cats_article.tpl",
+.urlencode["Site_Gostar/<article>"], exec("$return_var:0 = mgmt_cats_ovr_tpl"),
+			"cat_item" => "mgmt_cats_item.tpl",
+.urlencode["Site_Gostar/<article>"], exec("$return_var:0 = mgmt_cats_item_tpl"),
+
+		        "cat_caption" => "mgmt_cats_caption.tpl",
+.urlencode["Site_Gostar/<caption>"], exec("$return_var:0 = mgmt_cats_ovr_tpl"),
+			"cat_item" => "mgmt_cats_item.tpl",
+.urlencode["Site_Gostar/<caption>"], exec("$return_var:0 = mgmt_cats_item_tpl"),
+
+		        "cat_section" => "mgmt_cats_section.tpl",
+.urlencode["Site_Gostar/<section>"], exec("$return_var:0 = mgmt_cats_ovr_tpl"),
+			"cat_item" => "mgmt_cats_item.tpl",
+.urlencode["Site_Gostar/<section>"], exec("$return_var:0 = mgmt_cats_item_tpl"),
+
+		        "cat_aside" => "mgmt_cats_section.tpl",
+.urlencode["Site_Gostar/<aside>"], exec("$return_var:0 = mgmt_cats_ovr_tpl"),
+			"cat_item" => "mgmt_cats_item.tpl",
+.urlencode["Site_Gostar/<aside>"], exec("$return_var:0 = mgmt_cats_item_tpl"),
+
+fabrica.html("mgmt_app_Site_Gostar.tpl"); => mgmt_caption_tpl . mgmt_form_tpl,
+$aSQL = UPDATE Estoque SET Estocados_id = Produzidos_id WHERE  $id_id, $Código_id, $Modelo_id, $Cadeira_id, $Mesa_id, $Conjunto_id, $Custo_da_produção_id, Quantidade_id, $Data_id, $Total_Produzidos_id;
+
+$aSQL = "SELECT [DISTINC] Produzidos, AS $Produzidos_id FROM $id_id, $Código, $Modelo, $Cadeira_id, $Mesa_id, $Conjunto_id, $Custo_da_produçao_id, $Quantidade_id, $Data_id  from Produzidos WHERE Produzidos-id, id_id, Código_id, $Modelo_id, $Cadeira_id, $Mesa_id, $Conjunto_id, $Custo_da_produção_id $Quantidade_id = Estoque ORDER BY $Produzidos_id ASC )";
+ 
+$aSQL = insert into Produzidos_id (Produzidos_id, $id_id, $Código, $Modelo, $Cadeira_id, $Mesa_id, $Conjunto_id, $Custo_da_produçao_id, $Quantidade_id, $Data_id); Values('','','','','','','','','','');
+
+			$aDB->query( $aSQL );
+			if ($aDB->num_rows() > 0)
+			{
+			while ( $aDB->next_record() )
+				{				
+				$aCatID  = $aDB->f("$Id");
+				$aCatName = $aDB->f("Id");
+				$aCatID  = $aDB->f("$Modelo");
+				$aCatName  = $aDB->f($Modelo");
+				$aCatID  = $aDB->f("$Cadeira");
+				$aCatName  = $aDB->f("Cadeira");
+				$aCatID  = $aDB->f("$Mesa");
+				$aCatName  = $aDB->f("Mesa");
+				$aCatID  = $aDB->f("$Conjunto");
+				$aCatName  = $aDB->f("Conjunto");
+				$aCatID  = $aDB->f("$Custo_da_produção");
+				$aCatName  = $aDB->f("Custo_da_produçao");
+				$aCatID  = $aDB->f("$Quantidade_id");
+				$aCatName  = $aDB->f("$Quantidade");
+				$aCatID  = $aDB->f("$Data_id");
+				$aCatName  = $aDB->f("Data");
+				
+				$aTPL->assign( arrey("form_ID"  => $aCatID,
+							"CAT_NAME => $aCatName" ));
+				$aTPL->parse("PRODUZIDOS", ".cat_item" );
+				}
+				$aTPL->parse( "ESTOQUE" , "cat_form");
+				}
+			else
+			{
+			$aTPL->assign ( arrey("PRODUTOS_FABRICADOS" => "cat_caption" ));
+			}	
+			$aTPL->assign( arrey("TITLE" => "PRODUZIDOS",
+				"MARCHAND_NAME" => GetMerchandName, GetMechandID, GetMerchandMoney (
+				$aDB, $aProduzidos_id )
+				) );
+			$aTPL->Parse("TITLE" "title");	
+			$aTPL->Parse("PAGE", "base");
+			$aTPL->parse("PAGE_BODY", "page_body");
+			$aTPL->parse( "BODY", "body");
+			$aTPL->parse("FOOTER", "footer");
+			$aTPL->parse("HEADER", "header");
+			$aTPL->parse("ARTICLE", "article");
+			$aTPL->parse("CAPTION", "caption");
+			$aTPL->parse("SECTION", "section");
+			$aTPL->parse("ASIDE", "aside");
+			$aTPL->FastPrint("Produzidos");?> . <?php exec =parse->@$aDB ->assign($aSQL . $aTPL)?>
